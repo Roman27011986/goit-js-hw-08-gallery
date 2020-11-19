4b6acba66c646f43067a1c996f52b3d38d2330b5
Roman27011986 <>
2020-11-19 18:40:56 +0200

    fhf

--- gallery.js
+++ gallery.js
@@ -1,21 +1,24 @@
 import images from './gallery-items.js';
 const galleryRef = document.querySelector('.gallery');
+let targetIndex = 0;
 
-images.forEach(img => {
-      const galleryitemRef =document.createElement('li')
+images.forEach((img,i)=> {
+    const galleryitemRef =document.createElement('li')
      const gallerylinkRef = document.createElement('a')
     const galleryimageRef = document.createElement('img')
     galleryitemRef.classList.add('gallery__item')
     gallerylinkRef.classList.add('gallery__link')
     galleryimageRef.classList.add('gallery__image')
-    galleryimageRef.setAttribute('data-source',img.original)
+    galleryimageRef.setAttribute('data-source', img.original)
+    galleryimageRef.setAttribute('data-index',[i])
     galleryimageRef.src = img.preview
     galleryimageRef.alt = img.description
     gallerylinkRef.href = img.original
-
+    
     gallerylinkRef.appendChild(galleryimageRef)
     galleryitemRef.appendChild(gallerylinkRef)
-   galleryRef.appendChild(galleryitemRef)
+    galleryRef.appendChild(galleryitemRef)
+    
     
 })
 
@@ -25,16 +28,17 @@ const imgModal = document.querySelector('.lightbox__image');
 galleryRef.addEventListener('click',onGalleryClick )
 openModal.addEventListener('click', clsModal)
 
-window.addEventListener('keydown', ev => {
-    if(ev.code === 'Escape' ){
+window.addEventListener('keydown', event => {
+    if(event.code === 'Escape' ){
     openModal.classList.remove('is-open')
     }
 })
-function clsModal() {
-    
+function clsModal(event) {
+    if (event.target.nodeName === 'IMG') {
+        return
+    }
     openModal.classList.remove('is-open')
-    
-}
+    }
 
 
 function onGalleryClick(event) {
@@ -46,4 +50,26 @@ function onGalleryClick(event) {
     imgModal.src = ""
     imgModal.src = event.target.dataset.source
    
+    targetIndex = event.target.dataset.index
+    targetIndex = Number(targetIndex) 
+  
+   
 }
+
+ 
+    window.addEventListener('keydown', event => {
+    if (event.code === 'ArrowRight') {
+        targetIndex += 1
+        imgModal.src = images[targetIndex].original  
+    
+    }
+    })
+ window.addEventListener('keydown', event => {
+    if (event.code === 'ArrowLeft') {
+        targetIndex -= 1
+        imgModal.src = images[targetIndex].original 
+        
+    
+    }
+ })
+console.log(images)
