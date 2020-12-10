f6180ba9cc116778ff5989f65e7d2e2089007644
Roman27011986 <>
2020-12-10 23:43:00 +0200

    fix

--- gallery.js
+++ gallery.js
@@ -2,72 +2,66 @@ import images from './gallery-items.js';
 const galleryRef = document.querySelector('.gallery');
 let targetIndex = 0;
 
-const imgMap = images.map((img,i)=> {
-    const galleryitemRef = document.createElement('li')
-    const gallerylinkRef = document.createElement('a')
-    const galleryimageRef = document.createElement('img')
+const imgMap = images.map((img, i) => {
+  const galleryitemRef = document.createElement('li');
+  const gallerylinkRef = document.createElement('a');
+  const galleryimageRef = document.createElement('img');
 
-    galleryitemRef.classList.add('gallery__item')
-    gallerylinkRef.classList.add('gallery__link')
-    galleryimageRef.classList.add('gallery__image')
-    galleryimageRef.setAttribute('data-source', img.original)
-    galleryimageRef.setAttribute('data-index', [i])
-    
-    galleryimageRef.src = img.preview
-    galleryimageRef.alt = img.description
-    gallerylinkRef.href = img.original
-    
-    gallerylinkRef.appendChild(galleryimageRef)
-  return galleryitemRef.appendChild(gallerylinkRef)
-    
-})
-galleryRef.append(...imgMap)
+  galleryitemRef.classList.add('gallery__item');
+  gallerylinkRef.classList.add('gallery__link');
+  galleryimageRef.classList.add('gallery__image');
+  galleryimageRef.setAttribute('data-source', img.original);
+  galleryimageRef.setAttribute('data-index', [i]);
 
+  galleryimageRef.src = img.preview;
+  galleryimageRef.alt = img.description;
+  gallerylinkRef.href = img.original;
+
+  gallerylinkRef.appendChild(galleryimageRef);
+  return galleryitemRef.appendChild(gallerylinkRef);
+});
+galleryRef.append(...imgMap);
 
 const openModal = document.querySelector('.lightbox');
 const imgModal = document.querySelector('.lightbox__image');
 
-galleryRef.addEventListener('click',onGalleryClick )
-openModal.addEventListener('click', clsModal)
+galleryRef.addEventListener('click', onGalleryClick);
+openModal.addEventListener('click', clsModal);
 
 window.addEventListener('keydown', event => {
-    if(event.code === 'Escape' ){
-    openModal.classList.remove('is-open')
-    }
-})
+  if (event.code === 'Escape') {
+    openModal.classList.remove('is-open');
+  }
+});
 function clsModal(event) {
-    if (event.target.nodeName === 'IMG') {
-        return
-    }
-    openModal.classList.remove('is-open')
-    
-    }
-
+  if (event.target.nodeName === 'IMG') {
+    return;
+  }
+  openModal.classList.remove('is-open');
+}
 
 function onGalleryClick(event) {
-    event.preventDefault()
-    if (event.target.nodeName !== 'IMG') {
-        return
-    }
-    openModal.classList.add('is-open')
-    imgModal.src = ""
-    imgModal.src = event.target.dataset.source
-   
-    targetIndex = event.target.dataset.index
-    targetIndex = Number(targetIndex) 
-}
-  
-window.addEventListener('keydown', event => {
-    if (event.code === 'ArrowRight' && (images.length - 1) > targetIndex ) {
-        targetIndex += 1
-        imgModal.src = images[targetIndex].original  
-    }
-})
- window.addEventListener('keydown', event => {
-    if (event.code === 'ArrowLeft' &&  targetIndex > 0 ) {
-        targetIndex -= 1
-        imgModal.src = images[targetIndex].original 
-    } 
- })
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  openModal.classList.add('is-open');
+  imgModal.src = '';
+  imgModal.src = event.target.dataset.source;
 
- 
+  targetIndex = event.target.dataset.index;
+  targetIndex = Number(targetIndex);
+}
+
+window.addEventListener('keydown', event => {
+  if (event.code === 'ArrowRight' && images.length - 1 > targetIndex) {
+    targetIndex += 1;
+    imgModal.src = images[targetIndex].original;
+  }
+});
+window.addEventListener('keydown', event => {
+  if (event.code === 'ArrowLeft' && targetIndex > 0) {
+    targetIndex -= 1;
+    imgModal.src = images[targetIndex].original;
+  }
+});
